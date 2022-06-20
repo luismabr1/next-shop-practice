9a456658a9e94573af30abd17c1c6f080c158f0a
Luis Manuel Brito <lsbrt4@gmail.com>
2022-06-20 16:32:12 -0400

    agrego rutas y componentes del layout

---
+++ src/layout/MainLayout.js
@@ -0,0 +1,20 @@
+import Header from '@components/Header';
+import Nav from '@commons/Nav';
+
+const MainLayout = ({children}) => {
+    return (
+        <>  
+            <div className="min-h-full">
+                <Header />
+                <Nav />
+                <main>
+                    <div className="max-w-7xl mx-auto py-6 sm:px-8">
+                        {children}
+                    </div>
+                </main>
+            </div>
+        </>
+    );
+};
+
+export default MainLayout;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,8 @@
 /** @type {import('tailwindcss').Config} */
+
 const colors = require('tailwindcss/colors')
 module.exports = {
-  content: ['./src/**/*{html, js, jsx}'],
+  content: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
   theme: {
     colors: {
       ...colors
